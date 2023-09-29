a59e9c44fccc2ceb017cae71b79f786032723f2c
Rafther0112 <r.velasquez@uniandes.edu.co>
2023-09-29 11:45:39 -0500

    updates

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return open('completo_2.html').read()
+    return open('index.html').read()
 
 @app.route('/upload', methods=['POST'])
 def upload_audio():
