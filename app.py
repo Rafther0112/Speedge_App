497928f2a7dd5acff55a801b46e4395d67da7827
Rafther0112 <r.velasquez@uniandes.edu.co>
2023-09-29 11:47:22 -0500

    updates

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from Modulo_3_text_to_speech import generacion_audio
 from Modulo_4_sound_of_voice import reproduccion_audio
 
 app = Flask(__name__)
-#charge_model()
+charge_model()
 
 @app.route('/')
 def index():
