ee2234558733a98fc01010cd6372e70df96eb980
Rafther0112 <r.velasquez@uniandes.edu.co>
2023-11-02 20:31:16 -0500

    updates

--- SpeedgeBot.py
+++ SpeedgeBot.py
@@ -3,7 +3,7 @@ import logging
 #%%
 from telegram import Update, ForceReply, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
-
+#%%
 logger = logging.getLogger(__name__)
 
 # Store bot screaming status
